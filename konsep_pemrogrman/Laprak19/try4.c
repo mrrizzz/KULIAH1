c9f6b8fa61800ecb7364ebb390b508a9d4393c6f
muhammadrafirizaldi <muhammadrafiriz23@gmail.com>
2023-10-25 14:55:30 +0700

    laprak19

--- konsep_pemrogrman/Laprak19/try4.c
+++ konsep_pemrogrman/Laprak19/try4.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
 #include <math.h>
 #define PI 3.14159f
+
 float radian(int x)
 {
     return x * PI / 180.0f;
 }
+
 int main()
 {
     int x[] = {0, 30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330, 360};
     float rad[13];
+
     for (int i = 0; i < 13; i++)
     {
         rad[i] = radian(x[i]);
     }
 
     float data[13][3];
+
     for (int i = 0; i < 13; i++)
     {
         for (int j = 0; j < 3; j++)
@@ -31,12 +35,17 @@ int main()
 
             case 2:
                 data[i][j] = tan(rad[i]);
+                if (x[i] == 90 || x[i] == 270)
+                {
+                    data[i][j] = 0;
+                }
                 break;
             }
         }
     }
+
     for (int i = 0; i < 13; i++)
     {
-        printf("Sudut %d\t mempunyai Sin(x) = %.3f,\t cos(x) = %.3f,\t tan(x) = %.3f \n", x[i], data[i][0], data[i][1], data[i][2]);
+        printf("Sudut  %d   \tSin(x) = %.3f, \tcos(x) = %.3f,   \ttan(x) = %.3f \n", x[i], data[i][0], data[i][1], data[i][2]);
     }
 }
