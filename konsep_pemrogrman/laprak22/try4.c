5cac040b78f76c6341bc054573e9572a19af97ae
Muhammad Rafi Rizaldi <147369114+mrrizzz@users.noreply.github.com>
2023-11-03 02:56:39 +0000

    tes 4

---
+++ konsep_pemrogrman/laprak22/try4.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char input[100], input2[100];
+    printf("Masukkan kalimat 1 : ");
+    fgets(input, sizeof(input), stdin);
+    printf("Masukkan kalimat 2 : ");
+    fgets(input2, sizeof(input2), stdin);
+    printf("cek sensitif\n");
+    int banding = strcmp(input, input2);
+    printf("string 1 %s string 2", banding == 0 ? "sama dengan" : banding == 1 ? "lebih dari" : "kurang dari");
+    printf("\n");
+    printf("cek non sensitif\n");
+    int banding2 = strcmpi(input, input2);
+    printf("string 1 %s string 2", banding == 0 ? "sama dengan" : banding == 1 ? "lebih dari" : "kurang dari");
+    printf("\n");
+}
